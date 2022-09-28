b046d459ccd2d37b6283e6a68c141947f28eeb32
João Otavio <joaokorcz@hotmail.com>
2022-09-27 22:36:15 -0300

    finished checa_estoque function and publish to topic to request products

--- loja.py
+++ loja.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import pika
+import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-
 channel.queue_declare(queue='reposicao')
 
 class Loja:
     def __init__(self, nome):
         self.nome = nome
-        self.produto = []
+        self.produtos = []
     
     def adiciona_produto(self, nome, quantidade):
         if quantidade >= 100:
@@ -21,14 +21,14 @@ class Loja:
         else:
             classe = "C"
 
-        produto = {"nome": nome, "quantidade": quantidade, "classe": classe}
-        self.produto = self.produto.append(produto)
+        produto = { "nome": nome, "quantidade": quantidade, "classe": classe }
+        self.produtos.append(produto)
     
     def remove_produto(self, nome, quantidade):
-        #index = next((index for (index, d) in enumerate(self.produto) if d["nome"] == nome), None)
-        item = next(item for item in self.produto if item['nome'] == nome)
+        #index = next((index for (index, d) in enumerate(self.produtos) if d["nome"] == nome), None)
+        item = next(item for item in self.produtos if item['nome'] == nome)
 
-        if (item['quantidade'] - quantidade) <= 0:
+        if (item['quantidade'] - quantidade) < 0:
             print("Estoque insuficiente")
             return
 
@@ -37,37 +37,49 @@ class Loja:
             farol = self.checa_estoque(nome)
 
             if farol == "vermelho":
-                pedido = # ENVIAR NOME E QUANTIDADE, CALCULAR A QUANTIDADE COM BASE NA PORCENTAGEM DA CLASSE
-                channel.basic_publish(exchange='', routing_key='reposicao', body=pedido)
+                referencia = self.referencia_classe(item)
+                reabastecimento = referencia - item['quantidade']
+
+                pedido = { "nome": self.nome, "quantidade": reabastecimento }
+                channel.basic_publish(exchange='', routing_key='reposicao', body=json.dumps(pedido))
 
             return
 
     def checa_estoque(self, nome):
-        item = next(item for item in self.produto if item['nome'] == nome)
-        farol = item.quantidade * 100
+        item = next(item for item in self.produtos if item['nome'] == nome)
 
-        # FALTANDO IMPLEMENTAR
-        if item.classe == "A":
-            porcentagem = farol / 100
-
-        elif item.classe == "B":
-        
+        if item['classe'] == "A":
+            comparacao = 100
+        elif item['classe'] == "B":
+            comparacao = 60
         else:
+            comparacao = 20
 
-pedido = "item: iPhone, quantidade: 30"
+        porcentagem = item['quantidade'] * 100 / comparacao
+        
+        if porcentagem >= 50:
+            return "verde"
+
+        elif porcentagem >= 25:
+            return "amarelo"
+
+        return "vermelho"
+
+    def referencia_classe(self, produto):
+        if produto['classe'] == "A":
+            return 100
+        elif produto['classe'] == "B":
+            return 60
+        return 20
 
 americanas = Loja("Americanas")
+americanas.adiciona_produto("Notebook", 2)
+americanas.remove_produto("Notebook", 2)
 
-def callback(ch, method, properties, body):
-    
+""" def callback(ch, method, properties, body):
     print('Recebido:', body)
 
 channel.basic_consume(queue='fabrica', auto_ack=True, on_message_callback=callback)
-
-channel.start_consuming()
-
-channel.basic_publish(exchange='', routing_key='reposicao', body=pedido)
-
-print("Enviando pedido de iPhone!")
+channel.start_consuming() """
 
 connection.close()
